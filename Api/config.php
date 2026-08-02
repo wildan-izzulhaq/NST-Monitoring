d18cf29113aa4aaea17ead9e315f5aa8deb210de
Wildan Izzulhaq <wildanizzul13@gmail.com>
2026-08-02 17:00:30 +0700

    config.php

--- Api/config.php
+++ Api/config.php
@@ -1,17 +1,9 @@
 ﻿<?php
-// ================================================================
-//  config.php — Konfigurasi Database
-//  EDIT BAGIAN INI sesuai data dari cPanel Domainesia kamu
-// ================================================================
 
-define('DB_HOST', 'localhost');       // Jangan diubah (biasanya localhost)
-define('DB_USER', 'diagtemx_nst');     // << GANTI: username database dari cPanel
-define('DB_PASS', 'M+XD$hxrX~OqYlf*'); // << GANTI: password database dari cPanel
-define('DB_NAME', 'diagtemx_nst');     // << GANTI: nama database dari cPanel
-
-// ================================================================
-//  Jangan ubah kode di bawah ini
-// ================================================================
+define('DB_HOST', 'localhost');
+define('DB_USER', '');
+define('DB_PASS', ''); 
+define('DB_NAME', '');
 
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
